b66b6cf70200dbe95d4cde557405e058a48a455a
gluemagazine <126933672+gluemagazine@users.noreply.github.com>
2026-05-02 13:42:18 -0600

    added QOL method and a clarifying method for diagonals versus normal movement to move generator, started pawn generator

--- shared/src/main/java/chess/MoveGenerators/MoveGenerator.java
+++ shared/src/main/java/chess/MoveGenerators/MoveGenerator.java
@@ -7,9 +7,23 @@ public abstract class MoveGenerator {
 
     public abstract Collection<ChessMove> getMoves(ChessBoard board, ChessPosition myPosition, ChessPiece piece);
 
+    public ChessPosition getPosition(ChessPosition start, int[] dir,int magnitude){
+        return new ChessPosition(start.getRow() + dir[0] * magnitude, start.getColumn() + dir[1] * magnitude);
+    }
 
     public ChessPosition getDiagonal(ChessPosition start, int[] slope,int magnitude){
-        return new ChessPosition(start.getRow() + slope[0] * magnitude, start.getColumn() + slope[1] * magnitude);
+        return getPosition(start,slope,magnitude);
+    }
+
+    public boolean getAndCheck(ChessPosition destination,ChessBoard board,ChessPiece self){
+        if (!board.isValid(destination)){
+            return false;
+        }
+        ChessPiece at = board.getPiece(destination);
+        if(at == null){
+            return true;
+        }
+        return at.getTeamColor() == self.getTeamColor();
     }
 
 }
--- shared/src/main/java/chess/MoveGenerators/PawnMoveGenerator.java
+++ shared/src/main/java/chess/MoveGenerators/PawnMoveGenerator.java
@@ -1,12 +1,62 @@
 package chess.MoveGenerators;
 import chess.*;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
+
+
+
 
 public class PawnMoveGenerator extends MoveGenerator{
+
+    ArrayList<ChessPiece> promotions;
+
     @Override
     public Collection<ChessMove> getMoves(ChessBoard board, ChessPosition myPosition, ChessPiece piece) {
-        return List.of();
+        ArrayList<ChessMove> valid_moves = new ArrayList<>();
+        int[][] slopes = {{1,0}, {-1,0}};
+        int[][] white_diagonals = { {1,1}, {1,-1}};
+        int[][] black_diagonals = {{-1,1},{-1,-1}};
+        ChessPosition next;
+        switch(piece.getTeamColor()){
+            case BLACK:
+                next = getPosition(myPosition,slopes[1],1);
+                if(!board.isValid(next)) {
+                    break;
+                }
+                ChessPiece at = board.getPiece(next);
+                if (at == null){
+                    valid_moves.add(new ChessMove(myPosition, next,null));
+                    if (myPosition.getRow() == 7) {
+                        next = getPosition(myPosition, slopes[1], 2);
+                        if (board.isValid(next)) {
+                            valid_moves.add(new ChessMove(myPosition, next, null));
+                        }
+                    }
+                }
+                for(int[] diagonal : black_diagonals){
+                    next = getDiagonal(myPosition,diagonal,1);
+                    boolean result = getAndCheck(next,board,piece);
+                    if(result){
+                        valid_moves.add(new ChessMove(myPosition,next,null));
+                    }
+                }
+
+                break;
+            case WHITE:
+                next = getPosition(myPosition,slopes[0],1);
+                if(!board.isValid(next)){
+                    break;
+                }
+                valid_moves.add(new ChessMove(myPosition, next,null));
+                if (myPosition.getRow() == 2){
+                    next = getPosition(myPosition,slopes[1],2);
+                    if(board.isValid(next)){
+                        valid_moves.add(new ChessMove(myPosition, next,null));
+                    }
+                }
+                break;
+        }
+        return valid_moves;
     }
 }
