117f205f386443da0fa2b59f5fc3b164ecb2d13c
gluemagazine <126933672+gluemagazine@users.noreply.github.com>
2026-05-09 09:27:03 -0600

    Refactored the code for getting the positions and possible positions to ease the need for further refactoring, and ease of editing (forgot to commit the BishopMoveGenerator)

--- shared/src/main/java/chess/MoveGenerators/BishopMoveGenerator.java
+++ shared/src/main/java/chess/MoveGenerators/BishopMoveGenerator.java
@@ -1,30 +1,18 @@
 package chess.MoveGenerators;
 import chess.*;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 public class BishopMoveGenerator extends MoveGenerator{
     @Override
     public Collection<ChessMove> getMoves(ChessBoard board, ChessPosition pos) {
-        ArrayList<ChessMove> valid_positions = new ArrayList<>();
-        ChessPiece piece = board.getPiece(pos);
         int[][] dirs = {{-1,-1},{-1,1},{1,-1},{1,1}};
-        for(var dir: dirs){
-            for(int i = 1; i < 9; i ++){
-                ChessPosition new_pos = getPosFromVector(pos,dir,i);
-                if(validPosition(board,new_pos,piece)){
-                    ChessMove move = new ChessMove(pos,new_pos,null);
-                    valid_positions.add(move);
-                    if(board.getPiece(new_pos) != null){
-                        break;
-                    }
-                }
-                else{
-                    break;
-                }
-            }
-        }
-        return valid_positions;
+        return getMovesFromDirs(board,pos,dirs,8);
+    }
+
+    @Override
+    public Collection<ChessMove> getTheoreticalMoves(ChessBoard board, ChessPosition pos) {
+        int[][] dirs = {{-1,-1},{-1,1},{1,-1},{1,1}};
+        return getTheoreticalFromDirs(board,pos,dirs,8);
     }
 }
