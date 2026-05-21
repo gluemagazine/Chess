ed034f6f2ceef87774db00d6f4fe5a74e1ed5846
gluemagazine <126933672+gluemagazine@users.noreply.github.com>
2026-05-20 22:27:17 -0600

    implemented user and data service tests

--- server/src/test/java/service/DataServiceTest.java
+++ server/src/test/java/service/DataServiceTest.java
@@ -1,13 +1,25 @@
 package service;
 
+import dataaccess.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class DataServiceTest {
 
+    private DataService data;
+
+
+    @BeforeEach
+    void createServices(){
+        GameDAO games = new MemoryGameDAO();
+        AuthDAO auth = new MemoryAuthDAO();
+        UserDAO users = new MemoryUserDAO();
+        data = new DataService(auth,games,users);
+    }
+
     @Test
     void clear() {
-
+        data.clear();
     }
 }
--- server/src/test/java/service/UserServiceTest.java
+++ server/src/test/java/service/UserServiceTest.java
@@ -1,24 +1,119 @@
 package service;
 
+import dataaccess.*;
+import model.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceTest {
 
-    @Test
-    void login() {
+    private UserService user;
+    private DataService data;
+
+    @BeforeEach
+    void createServices(){
+        GameDAO games = new MemoryGameDAO();
+        AuthDAO auth = new MemoryAuthDAO();
+        UserDAO users = new MemoryUserDAO();
+        user = new UserService(users,auth);
+        data = new DataService(auth,games,users);
     }
 
     @Test
-    void register() {
+    void loginSuccessful() {
+        try {
+            RegisterResult result = user.register(new RegisterRequest("ExistingUser","password","example"));
+            user.logout(new LogoutRequest(result.authToken()));
+            LoginResult newResult = user.login(new LoginRequest("ExistingUser","password"));
+            Assertions.assertNotNull(newResult.authToken());
+            Assertions.assertEquals("ExistingUser",newResult.username());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    void loginNonExistentUser(){
+        Assertions.assertThrows(DataAccessException.class, ()-> user.login(new LoginRequest("ExistingUser","password")));
+    }
+
+    @Test
+    void loginBadCredentials(){
+        try {
+            RegisterResult result = user.register(new RegisterRequest("ExistingUser","password","example"));
+            user.logout(new LogoutRequest(result.authToken()));
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        Assertions.assertThrows(DataAccessException.class, ()-> user.login(new LoginRequest("ExistingUser","badPassword")));
+    }
+
+    @Test
+    void loginBadRequest(){
+        try {
+            RegisterResult result = user.register(new RegisterRequest("ExistingUser","password","example"));
+            user.logout(new LogoutRequest(result.authToken()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        Assertions.assertThrows(BadCredentialsException.class, ()-> user.login(new LoginRequest(null,"null")));
+    }
+
+    @Test
+    void registerSuccessful() {
+        try {
+            RegisterResult result = user.register(new RegisterRequest("ExistingUser","password","example"));
+            Assertions.assertNotNull(result.authToken());
+            Assertions.assertEquals("ExistingUser",result.username());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void registerExistingUser(){
+        try {
+            RegisterResult result = user.register(new RegisterRequest("ExistingUser","password","example"));
+            Assertions.assertNotNull(result.authToken());
+            Assertions.assertEquals("ExistingUser",result.username());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        Assertions.assertThrows(AlreadyTakenException.class, ()->user.register(new RegisterRequest("ExistingUser","password","example")));
+    }
+
+    @Test
+    void registerBadRequest(){
+        Assertions.assertThrows(BadCredentialsException.class, ()->user.register(new RegisterRequest(null,"password","example")));
+        Assertions.assertThrows(BadCredentialsException.class, ()->user.register(new RegisterRequest("ExistingUser","password",null)));
+        Assertions.assertThrows(BadCredentialsException.class, ()->user.register(new RegisterRequest("ExistingUser",null,"example")));
     }
 
     @Test
     void logout() {
+        try {
+            RegisterResult result = user.register(new RegisterRequest("ExistingUser","password","example"));
+            user.logout(new LogoutRequest(result.authToken()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Test
     void clear() {
+        try {
+            RegisterResult result = user.register(new RegisterRequest("ExistingUser","password","example"));
+            Assertions.assertNotNull(result.authToken());
+            Assertions.assertEquals("ExistingUser",result.username());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        user.clear();
+        data.clear();
+        Assertions.assertThrows(DataAccessException.class, ()-> user.login(new LoginRequest("ExistingUser","password")));
     }
 }
