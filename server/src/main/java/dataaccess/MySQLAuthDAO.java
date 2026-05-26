0dffe1efc5f47ee4d076672574d7bfd8a7844326
gluemagazine <126933672+gluemagazine@users.noreply.github.com>
2026-05-25 22:01:55 -0600

    passed positive AuthDaoTests

--- server/src/main/java/dataaccess/MySQLAuthDAO.java
+++ server/src/main/java/dataaccess/MySQLAuthDAO.java
@@ -5,7 +5,10 @@ import dataaccess.Exceptions.ResponseException;
 import model.AuthData;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.UUID;
 
 public class MySQLAuthDAO implements AuthDAO{
@@ -23,49 +26,131 @@ public class MySQLAuthDAO implements AuthDAO{
     }
 
     @Override
-    public void clear() {
-//        Connection connection = null;
-//        try {
-//            connection = DatabaseManager.getConnection();
-//        } catch (SQLException e){
-//            if(connection != null && !connection.isClosed()){
-//                connection.rollback();
-//            }
-//
-//        }
+    public void clear() throws DataAccessException {
+        Connection connection = null;
+        try {
+            connection = DatabaseManager.getConnection();
+            connection.setAutoCommit(false);
+
+            String sql = "delete from auth";
+
+            try(PreparedStatement stmt = connection.prepareStatement(sql)) {
+                stmt.executeUpdate();
+            }
+
+            connection.commit();
+        } catch (SQLException e){
+            try {
+                if (connection != null && !connection.isClosed()) {
+                    connection.rollback();
+                }
+            } catch (SQLException ex) {
+                throw new DataAccessException(ex.getMessage(),ex);
+            }
+            throw new DataAccessException(e.getMessage(),e);
+        }
     }
 
     @Override
     public AuthData getAuthFromToken(String authToken) throws DataAccessException {
         /*
         Insert code to get this from the database
-         */ 
-        return null;
+         */
+        Connection connection = null;
+        try {
+            connection = DatabaseManager.getConnection();
+
+            HashMap<String,String> tokens = new HashMap<>();
+
+            String sql = "select authToken, username from auth";
+
+            try(PreparedStatement stmt = connection.prepareStatement(sql);
+                ResultSet rs = stmt.executeQuery()) {
+
+                while(rs.next()) {
+                    tokens.put(rs.getString("authtoken"),rs.getString("username"));
+                }
+            }
+
+            if(tokens.containsKey(authToken)){
+                return new AuthData(authToken,tokens.get(authToken));
+            }
+            return null;
+
+        } catch (SQLException e){
+
+            throw new DataAccessException(e.getMessage(),e);
+        }
     }
 
     @Override
     public String createAuth(String username) throws DataAccessException {
         String token = generateToken();
 
-        /*
-        Insert code to put this into the database
-         */
+        Connection connection = null;
+        try {
+            connection = DatabaseManager.getConnection();
+            connection.setAutoCommit(false);
+
+            String sql = "insert into auth (authToken, username) values (?, ?)";
+
+            try(PreparedStatement stmt = connection.prepareStatement(sql)) {
+
+                stmt.setString(1, token);
+                stmt.setString(2, username);
+
+                stmt.executeUpdate();
+
+            }
+
+            connection.commit();
+        } catch (SQLException e){
+            try {
+                if (connection != null && !connection.isClosed()) {
+                    connection.rollback();
+                }
+            } catch (SQLException ex) {
+                return null;
+            }
+            return null;
+        }
 
         return token;
     }
 
     @Override
     public void deleteAuth(AuthData data) throws DataAccessException {
-        /*
-        Insert code to remove this from the database
-         */
+        Connection connection = null;
+        try {
+            connection = DatabaseManager.getConnection();
+            connection.setAutoCommit(false);
+
+            String sql = "DELETE FROM auth WHERE authToken = ?;";
+
+            try(PreparedStatement stmt = connection.prepareStatement(sql)) {
+                stmt.setString(1,data.authToken());
+                stmt.executeUpdate();
+            }
+
+            connection.commit();
+        } catch (SQLException e){
+            try {
+                if (connection != null && !connection.isClosed()) {
+                    connection.rollback();
+                }
+            } catch (SQLException ex) {
+                throw new DataAccessException(ex.getMessage(),ex);
+            }
+            throw new DataAccessException(e.getMessage(),e);
+        }
     }
 
     private final String[] createStatements = {
             """
             CREATE TABLE `auth` (
               `authToken` varchar(200) NOT NULL,
-              `username` varchar(100) NOT NULL
+              `username` varchar(100) NOT NULL,
+              PRIMARY KEY (`authToken`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
             """
     };
---
+++ server/src/test/java/dataaccess/AuthDAOTests.java
@@ -0,0 +1,75 @@
+package dataaccess;
+
+import dataaccess.Exceptions.DataAccessException;
+import model.AuthData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AuthDAOTests {
+    private MySQLAuthDAO auth;
+    @BeforeEach
+    void createAuthDAO(){
+        auth = new MySQLAuthDAO();
+    }
+
+    @Test
+    public void createAuthToken(){
+        String token;
+        try {
+             token = auth.createAuth("Testing123");
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        Assertions.assertNotNull(token);
+        System.out.println(token);
+    }
+
+    @Test
+    public void clearAuth(){
+        String[] tokens = new String[3];
+        try {
+            for (int i = 0; i < 3; i ++){
+                tokens[i] = auth.createAuth("Testing" + i);
+            }
+            auth.clear();
+            for (int i = 0; i < 3; i ++){
+                Assertions.assertNull(auth.getAuthFromToken(tokens[i]));
+            }
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Test
+    public void getAuthData(){
+        String token;
+        AuthData data;
+        try {
+            token = auth.createAuth("Testing123");
+             data = auth.getAuthFromToken(token);
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        Assertions.assertNotNull(token);
+        Assertions.assertNotNull(data);
+        System.out.println(token);
+        System.out.println(data);
+    }
+
+    @Test
+    public void deleteAuthData(){
+        String token;
+        try {
+            token = auth.createAuth("Testing123");
+            auth.deleteAuth(auth.getAuthFromToken(token));
+            Assertions.assertNull(auth.getAuthFromToken(token));
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+        Assertions.assertNotNull(token);
+        System.out.println(token);
+    }
+
+}
