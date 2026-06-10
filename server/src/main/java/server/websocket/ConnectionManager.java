049d222582db79e45057d59f53591725b23343c5
gluemagazine <126933672+gluemagazine@users.noreply.github.com>
2026-06-09 21:28:26 -0600

    added connection manager

---
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -0,0 +1,39 @@
+package server.websocket;
+
+
+import jakarta.websocket.Session;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConnectionManager {
+    private final ConcurrentHashMap<Integer, ArrayList<Session>> sessions = new ConcurrentHashMap<>();
+
+    public void addToGame(int gameID, Session session){
+        if(!sessions.containsKey(gameID)){
+            sessions.put(gameID, new ArrayList<>());
+        }
+        sessions.get(gameID).add(session);
+    }
+
+    public void removeFromGame(int gameID, Session session){
+        if(!sessions.containsKey(gameID)){
+            return;
+        }
+        sessions.get(gameID).remove(session);
+    }
+
+
+    public void broadcast(int gameID, Session excludeSession, ServerMessage message) throws IOException {
+        String msg = message.toString();
+        for (Session c : sessions.get(gameID)) {
+            if (c.isOpen()) {
+                if (!c.equals(excludeSession)) {
+                    c.getBasicRemote().sendText(msg);
+                }
+            }
+        }
+    }
+}
